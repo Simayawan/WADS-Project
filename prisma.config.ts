53af9a58146310088d9120ebc00a885a9e0b45af
Simayawan <allenxavinzky.adjiewibowo@binus.ac.id>
2026-06-14 17:51:03 +0700

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    // FIX LATER, THIS IS A SECURITY RISK, FOR NOW IT IS TEMPORARY
-    url: "postgresql://neondb_owner:npg_Ex4adS0DFTgX@ep-crimson-dust-aofjvsj8-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+
+    url: process.env.DATABASE_URL,
   },
 });
